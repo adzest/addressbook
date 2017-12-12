8651359227b695c19ba633ad561a79475f82c624
Alexey Isakov <isakov.ktk@gmail.com>
2017-12-12 20:13:36 +0200

    Extract 'type' text  method

--- src/test/java/com/java4qa/addressbook/appmanager/ApplicationManager.java
+++ src/test/java/com/java4qa/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,5 @@
 package com.java4qa.addressbook.appmanager;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,9 +9,11 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
     ChromeDriver wd;
 
+    private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private AddNewHelper addNewHelper;
     private GroupHelper groupHelper;
+
 
     public static boolean isAlertPresent(FirefoxDriver wd) {
         try {
@@ -27,24 +28,19 @@ public class ApplicationManager {
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
+        sessionHelper = new SessionHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         groupHelper = new GroupHelper(wd);
         addNewHelper = new AddNewHelper(wd);
-        loginToWebApp("admin", "secret");
-    }
-
-    private void loginToWebApp(String username, String password) {
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+        sessionHelper.loginToWebApp("admin", "secret");
     }
 
     public void stop() {
         wd.quit();
+    }
+
+    public SessionHelper sessionHelper() {
+        return sessionHelper;
     }
 
     public GroupHelper getGroupHelper() {
