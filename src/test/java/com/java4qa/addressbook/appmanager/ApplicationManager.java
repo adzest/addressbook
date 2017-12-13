b13302dbd354b2996962d45e487ada9ca6ca3b46
Alexey Isakov <isakov.ktk@gmail.com>
2017-12-13 19:25:38 +0200

    Remove drivers by super class WebDriver

--- src/test/java/com/java4qa/addressbook/appmanager/ApplicationManager.java
+++ src/test/java/com/java4qa/addressbook/appmanager/ApplicationManager.java
@@ -1,27 +1,41 @@
 package com.java4qa.addressbook.appmanager;
 
-import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-    ChromeDriver wd;
+    WebDriver wd;
 
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
-    private AddNewHelper addNewHelper;
+    private ContactHelper contactHelper;
     private GroupHelper groupHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new ChromeDriver();
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+        }
+
+        // wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         sessionHelper = new SessionHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         groupHelper = new GroupHelper(wd);
-        addNewHelper = new AddNewHelper(wd);
+        contactHelper = new ContactHelper(wd);
         sessionHelper.loginToWebApp("admin", "secret");
     }
 
@@ -37,8 +51,8 @@ public class ApplicationManager {
         return groupHelper;
     }
 
-    public AddNewHelper getAddNewHelper() {
-        return addNewHelper;
+    public ContactHelper getContactHelper() {
+        return contactHelper;
     }
 
     public NavigationHelper getNavigationHelper() {
