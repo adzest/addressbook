31a13df1d924f51dcefda467412d7d27c2a1ce9e
Alexey Isakov <isakov.ktk@gmail.com>
2017-12-15 17:46:44 +0200

    Chapter 5 Video 1: Run all test in one browser.

--- src/test/java/com/java4qa/addressbook/tests/TestBase.java
+++ src/test/java/com/java4qa/addressbook/tests/TestBase.java
@@ -2,21 +2,21 @@ package com.java4qa.addressbook.tests;
 
 import com.java4qa.addressbook.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
-    @BeforeMethod
-    public void setUp() throws Exception {
-        app.init();
-    }
+  @BeforeSuite
+  public void setUp() throws Exception {
+    app.init();
+  }
 
-    @AfterMethod
-    public void tearDown() {
-        app.stop();
-    }
+  @AfterSuite
+  public void tearDown() {
+    app.stop();
+  }
 
 }
